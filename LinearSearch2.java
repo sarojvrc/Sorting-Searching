d6ef5f97c7d2522dee9c5f70bacccb5d70f11cca
Saroj Kumar <53892172+sarojvrc@users.noreply.github.com>
2021-08-13 12:39:31 +0530

    Linear Search (Return the element)
    If the element is found, it will return the element itself otherwise it will return -1

---
+++ LinearSearch2.java
@@ -0,0 +1,49 @@
+package dsa;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LinearSearch2 {
+
+	//function for linearSearch which return the element
+		static int linearSearch(int arr[], int target) {
+			//now lets check if the size of the array is 0, then it will return -1
+			if(arr.length == 0) {
+				return -1;
+			}
+			//run a for loop for search
+			for(int i=0; i<arr.length; i++) { //here i means index value of the array
+				int element = arr[i]; 
+				if(element == target) {
+					return element;
+					//if we found the target element from the array then it will return the element
+					//of this array. 
+				}
+			} //for loop ends here
+			
+			//if we won't found the target element then return -1
+			return -1;
+			
+		}
+		//driver code
+		public static void main(String args[]) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the array size ");
+			int size = sc.nextInt();
+			System.out.println("Enter the array elements");
+			int arr[] = new int[size];
+			for(int i=0; i<arr.length; i++) {
+				arr[i] = sc.nextInt();
+			}
+			System.out.println("Array is "+Arrays.toString(arr));
+			
+			//lets get the target number from the user
+			System.out.println("Enter the target search number");
+			int target = sc.nextInt();
+			//now lets call the linear search function to find the search result
+			int result = linearSearch(arr, target);
+			System.out.println("The element is "+result);
+			//if it return -1, then target element is not found. 
+		}
+
+}
